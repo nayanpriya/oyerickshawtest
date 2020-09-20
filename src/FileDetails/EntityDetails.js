59d6bfff38bb9852d825ea190b90186d44f10dc1
Nayan Priya <nnayanpriya@gmail.com>
2020-09-20 14:15:25 +0530

    add packaeges

--- src/FileDetails/EntityDetails.js
+++ src/FileDetails/EntityDetails.js
@@ -5,129 +5,138 @@ import { getColumnNames } from "../datastore";
 import RejectModal from "./RejectModal";
 
 export default class EntityDetails extends React.Component {
-  state = {
-    rejectModalShow: false,
-    rejectRemark: "",
-    rejectId: null,
-    checkedEarningIds: []
-  };
+    state = {
+        rejectModalShow: false,
+        rejectRemark: "",
+        rejectId: null,
+        checkedEarningIds: []
+    };
 
-  approveEntity = () => {
-    this.props.approve(this.state.checkedEarningIds);
-  };
+    approveEntity = () => {
+        this.props.approve(this.state.checkedEarningIds);
+    };
 
-  rejectModalShow = (earning_id) => {
-    this.setState({ rejectModalShow: true, rejectId: earning_id });
-  };
+    rejectModalShow = (earning_id) => {
+        this.setState({ rejectModalShow: true, rejectId: earning_id });
+    };
 
-  rejectReasonChange = (event) => {
-    this.setState({ rejectRemark: event.target.value });
-  };
+    rejectReasonChange = (event) => {
+        this.setState({ rejectRemark: event.target.value });
+    };
 
-  rejectModalSubmit = () => {
-    this.props
-      .reject(this.state.rejectId, this.state.rejectRemark)
-      .then(this.rejectModalCancel);
-  };
+    rejectModalSubmit = () => {
+        const checkedEarningIds = this.state.checkedEarningIds;
+        var earning_id = this.state.rejectId;
+        const isEarningIdIsAlreadyChecked = checkedEarningIds.includes(earning_id);
+        if (isEarningIdIsAlreadyChecked) {
+            const newCheckedEarningIds = checkedEarningIds.filter(
+                (id) => id !== earning_id
+            );
+            this.setState({ checkedEarningIds: newCheckedEarningIds });
+        }
+        this.props
+            .reject(this.state.rejectId, this.state.rejectRemark)
+            .then(this.rejectModalCancel);
+    };
 
-  rejectModalCancel = () => {
-    this.setState({ rejectModalShow: false, rejectRemark: "", rejectId: null });
-  };
+    rejectModalCancel = () => {
+        this.setState({ rejectModalShow: false, rejectRemark: "", rejectId: null });
+    };
 
-  selectRow = (event, earning_id) => {
-    const isChecked = event.target.checked;
-    const checkedEarningIds = this.state.checkedEarningIds;
-    const isEarningIdIsAlreadyChecked = checkedEarningIds.includes(earning_id);
-    if (isChecked && !isEarningIdIsAlreadyChecked) {
-      checkedEarningIds.push(earning_id);
-      this.setState({ checkedEarningIds });
-    } else if (!isChecked && isEarningIdIsAlreadyChecked) {
-      const newCheckedEarningIds = checkedEarningIds.filter(
-        (id) => id !== earning_id
-      );
-      this.setState({ checkedEarningIds: newCheckedEarningIds });
-    }
-  };
+    selectRow = (event, earning_id) => {
+        const isChecked = event.target.checked;
+        const checkedEarningIds = this.state.checkedEarningIds;
+        const isEarningIdIsAlreadyChecked = checkedEarningIds.includes(earning_id);
+        if (isChecked && !isEarningIdIsAlreadyChecked) {
+            checkedEarningIds.push(earning_id);
+            this.setState({ checkedEarningIds });
+        } else if (!isChecked && isEarningIdIsAlreadyChecked) {
+            const newCheckedEarningIds = checkedEarningIds.filter(
+                (id) => id !== earning_id
+            );
+            this.setState({ checkedEarningIds: newCheckedEarningIds });
+        }
+    };
 
-  render() {
-    const { entityDetails } = this.props;
+    render() {
+        const { entityDetails } = this.props;
 
-    return (
-      this.props.uploadStatus === 2 &&
-      entityDetails && (
-        <div className="entity-details">
-          <div className="approve-btn-row">
-            <button
-              className="btn approve-btn"
-              onClick={this.approveEntity}
-              disabled={this.state.checkedEarningIds.length === 0}
-            >
-              Approve
-            </button>
-          </div>
-
-          <div className="table">
-            <div className="table-row thead">
-              <div className="table-column select">{""}</div>
-              {getColumnNames().map((col) => (
-                <div key={col} className="table-column">
-                  {col === "earning_id" ? "EARNING-ID" : col.toUpperCase()}
-                </div>
-              ))}
-              <div className="table-column action">Action</div>
-            </div>
-            {entityDetails && (
-              <div>
-                {entityDetails.map((row, index) => {
-                  return (
-                    <div className="table-row" key={row.earning_id}>
-                      <input
-                        className="table-column select"
-                        type="checkbox"
-                        onChange={(event) =>
-                          this.selectRow(event, row.earning_id)
-                        }
-                      />
-                      <div className="table-column">{row.mobile}</div>
-                      <div className="table-column">{row.earning_id}</div>
-                      <div className="table-column">{row.earning}</div>
-                      <div className="table-column action">
+        return (
+            this.props.uploadStatus === 2 &&
+            entityDetails && (
+                <div className="entity-details">
+                    <div className="approve-btn-row">
                         <button
-                          onClick={() => this.rejectModalShow(row.earning_id)}
-                          className="btn reject-btn"
+                            className="btn approve-btn"
+                            onClick={this.approveEntity}
+                            disabled={this.state.checkedEarningIds.length === 0}
                         >
-                          Reject
-                        </button>
-                      </div>
+                            Approve
+            </button>
                     </div>
-                  );
-                })}
-              </div>
-            )}
-          </div>
-          {this.state.rejectModalShow && (
-            <RejectModal>
-              <div className="reject-modal">
-                <textarea onChange={this.rejectReasonChange}></textarea>
-                <div className="reject-actions">
-                  <button
-                    className="btn reject-btn"
-                    onClick={this.rejectModalSubmit}
-                  >
-                    Reject
+
+                    <div className="table">
+                        <div className="table-row thead">
+                            <div className="table-column select">{""}</div>
+                            {getColumnNames().map((col) => (
+                                <div key={col} className="table-column">
+                                    {col === "earning_id" ? "EARNING-ID" : col.toUpperCase()}
+                                </div>
+                            ))}
+                            <div className="table-column action">Action</div>
+                        </div>
+                        {entityDetails && (
+                            <div>
+                                {entityDetails.map((row, index) => {
+                                    return (
+                                        <div className="table-row" key={row.earning_id}>
+                                            <input
+                                                className="table-column select"
+                                                type="checkbox"
+                                                onChange={(event) =>
+                                                    this.selectRow(event, row.earning_id)
+                                                }
+                                            />
+                                            <div className="table-column">{row.mobile}</div>
+                                            <div className="table-column">{row.earning_id}</div>
+                                            <div className="table-column">{row.earning}</div>
+                                            <div className="table-column action">
+                                                <button
+                                                    onClick={() => this.rejectModalShow(row.earning_id)}
+                                                    className="btn reject-btn"
+                                                >
+                                                    Reject
+                        </button>
+                                            </div>
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                        )}
+                    </div>
+                    {this.state.rejectModalShow && (
+                        <RejectModal>
+                            <div className="reject-modal">
+                                <textarea onChange={this.rejectReasonChange}></textarea>
+                                <div className="reject-actions">
+                                    <button
+                                        className="btn reject-btn"
+                                        onClick={this.rejectModalSubmit}
+                                    >
+                                        Reject
                   </button>
-                  <button
-                    className="btn cancel-btn"
-                    onClick={this.rejectModalCancel}
-                  >
-                    Cancel
+                                    <button
+                                        className="btn cancel-btn"
+                                        onClick={this.rejectModalCancel}
+                                    >
+                                        Cancel
                   </button>
+                                </div>
+                            </div>
+                        </RejectModal>
+                    )}
                 </div>
-              </div>
-            </RejectModal>
-          )}
-        </div>
-      )
-    );
-  }
+            )
+        );
+    }
 }
